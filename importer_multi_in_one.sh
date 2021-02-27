336976150d6a20ec1fced943a641d80004df94d8
Sixing Huang <dgg321982@gmail.com>
2021-02-27 11:49:56 +0100

    add metabat and concoct for in_one

--- importer_multi_in_one.sh
+++ importer_multi_in_one.sh
@@ -89,14 +89,113 @@ run_MaxBin.pl -contig $megahitout/final.contigs.fa -out $maxbinout  -reads_list 
 fi
 
 
-checkmfolder="${inputfolder}/${projectname}_mix_checkm"
+bamoutfolder="${inputfolder}/${projectname}_mix_bam"
+metabatoutfolder="${inputfolder}/${projectname}_mix_metabat"
+concoctoutfolder="${inputfolder}/${projectname}_mix_concoct"
+dasoutfolder="${inputfolder}/${projectname}_mix_das"
 
-if [ ! -d $checkmfolder ];
+das_checkmfolder="${inputfolder}/${projectname}_mix_das_checkm"
+maxbin_checkmfolder="${inputfolder}/${projectname}_mix_maxbin_checkm"
+metabat_checkmfolder="${inputfolder}/${projectname}_mix_metabat_checkm"
+concoct_checkmfolder="${inputfolder}/${projectname}_mix_concoct_checkm"
+
+
+
+
+if [ ! -d $bamoutfolder ];
+then
+mkdir $bamoutfolder
+
+bowtie2-build $megahitout/final.contigs.fa $megahitout/final.contigs && bowtie2 -x $megahitout/final.contigs -1 $R1_list  -2 $R2_list  | samtools view -bS -o $bamoutfolder/mapping.bam
+
+samtools sort -o $bamoutfolder/mapping_sort.bam $bamoutfolder/mapping.bam  && samtools index $bamoutfolder/mapping_sort.bam
+
+fi
+
+
+if [ ! -d $metabatoutfolder ];
+then
+mkdir $metabatoutfolder
+
+cd $metabatoutfolder
+
+runMetaBat.sh -m 1500 $megahitout/final.contigs.fa $bamoutfolder/mapping_sort.bam
+
+fi
+
+
+if [ ! -d $concoctoutfolder ];
+then
+mkdir $concoctoutfolder
+
+mkdir $concoctoutfolder/bin
+
+cd $concoctoutfolder
+
+cut_up_fasta.py $megahitout/final.contigs.fa -c 10000 -o 0 --merge_last -b contigs_10K.bed > contigs_10K.fa
+
+
+concoct_coverage_table.py contigs_10K.bed $bamoutfolder/mapping_sort.bam > coverage_table.tsv
+
+
+concoct --composition_file contigs_10K.fa --coverage_file coverage_table.tsv --threads 8
+
+merge_cutup_clustering.py clustering_gt1000.csv > clustering_merged.csv
+
+
+extract_fasta_bins.py $megahitout/final.contigs.fa clustering_merged.csv --output_path $concoctoutfolder/bin
+
+fi
+
+
+if [ ! -d $dasoutfolder ];
+then
+mkdir $dasoutfolder
+
+cd $maxbinoutfolder
+Fasta_to_Scaffolds2Bin.sh -e fasta > $dasoutfolder/maxbin.tsv
+
+echo "$metabatoutfolder/final.contigs.fa.metabat-bins1500"
+
+cd $metabatoutfolder/final.contigs.fa.metabat-bins1500
+Fasta_to_Scaffolds2Bin.sh -e fa > $dasoutfolder/metabat.tsv
+
+cd $concoctoutfolder/bin
+Fasta_to_Scaffolds2Bin.sh -e fa > $dasoutfolder/concoct.tsv
+
+DAS_Tool -i $dasoutfolder/maxbin.tsv,$dasoutfolder/metabat.tsv,$dasoutfolder/concoct.tsv -l maxbin,metabat,concoct -c $megahitout/final.contigs.fa -o $dasoutfolder/o -t 16 --search_engine diamond --write_bins
+
+fi
+
+
+o_bins=$dasoutfolder/o_DASTool_bins
+
+
+ if [ ! -d $das_checkmfolder ];
+ then
+
+ checkm  lineage_wf -t 8 -x fa $o_bins $das_checkmfolder
+
+ fi
+
+o_bins=$maxbinoutfolder
+if [ ! -d $maxbin_checkmfolder ];
 then
 
-source ~/anaconda3/etc/profile.d/conda.sh
 
-conda activate checkm
+checkm  lineage_wf -t 8 -x fasta $o_bins $maxbin_checkmfolder
+fi
 
-checkm  lineage_wf -t $cpu -x fasta $maxbinoutfolder $checkmfolder
+o_bins=$metabatoutfolder/final.contigs.fa.metabat-bins1500
+if [ ! -d $metabat_checkmfolder ];
+then
+
+checkm  lineage_wf -t 8 -x fa $o_bins $metabat_checkmfolder
+fi
+
+o_bins=$concoctoutfolder/bin
+if [ ! -d $concoct_checkmfolder ];
+then
+
+checkm  lineage_wf -t 8 -x fa $o_bins $concoct_checkmfolder
 fi
